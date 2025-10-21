99ce2f38c6861dd16c6409b95fe72b3f2819b633
workrblaiims <workrblaiims@gmail.com>
2025-10-21 22:06:39 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+
+// Service Worker for MD Medicine Quiz Master PWA
+const CACHE_NAME = 'md-quiz-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './questions.json'
+];
+
+// Install event - cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
